8a515cf37449c9d47ca58a62c2510720cdf5346b
romanornr <romanornr@gmail.com>
2018-01-18 21:29:38 +0100

    [migrations] create stores

---
+++ database/migrations/2018_01_16_142550_create_stores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('website');
+            $table->unsignedTinyInteger('min_confirmations');
+            $table->string('master_public_key');
+            $table->string('address_type');
+            $table->timestamps();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
