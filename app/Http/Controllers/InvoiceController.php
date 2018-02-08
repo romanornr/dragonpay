3e05f54d922451e36520b8f2aa575b1a9af24d0a
romanornr <romanornr@gmail.com>
2018-02-08 16:32:38 +0100

    test create invoice controller

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoices;
 
 class InvoiceController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        return Invoices::create($request->all());
+    }
 }
