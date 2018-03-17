3c9bb1c98aa594fe675228cd7faa84e751ff5726
romanornr <romanornr@gmail.com>
2018-03-17 12:57:53 +0100

    Store unit test

---
+++ database/factories/StoresFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+use Faker\Generator as Faker;
+use App\User;
+
+$factory->define(App\Models\Stores::class, function (Faker $faker){
+   return [
+       'name' => $faker->name,
+       'website' => $faker->unique()->name,
+       'min_confirmations' => $faker->numberBetween(0, 200),
+       'expiration_time' => $faker->numberBetween(15, 500),
+       'user_id' => factory(User::class)->create()->id
+   ];
+});
--- tests/Unit/StoreTest.php
+++ tests/Unit/StoreTest.php
@@ -1,23 +1,49 @@
 <?php
-//
-//namespace Tests\Unit;
-//
-//use Tests\TestCase;
-//use App\User;
-//use Illuminate\Foundation\Testing\WithFaker;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//
-//class StoreTest extends TestCase
-//{
-//    /**
-//     * A basic test example.
-//     *
-//     * @return void
-//     */
-//    public function testExample()
-//    {
-////        return dd($user = User::find(1));
-////        $response = $this->get('/stores');
-////        $response->assertStatus(201);
-//    }
-//}
+
+namespace Tests\Unit;
+
+use App\Models\Stores;
+use Prophecy\Comparator\Factory;
+use Tests\TestCase;
+use App\User;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class StoreTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A user can login, go to the create stores page
+     * and create a store
+     */
+    public function testCreateStoreAsLoggedInUser()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'secret'
+        ]);
+        $response->assertStatus(302);
+        $response = $this->get('/stores/create');
+        $response->assertStatus(200);
+
+        $response2 = $this->post('/stores', [
+            'name' => 'store1',
+            'website' => 'https://store1example.com',
+            'min_confirmations' => 1,
+            'expiration_time' => 15,
+            'user_id' => $user->id
+        ]);
+
+        $response2->assertStatus(302);
+
+        $store = Stores::find(1);
+        self::assertEquals($store->name, 'store1');
+        self::assertEquals($store->website, 'https://store1example.com');
+        self::assertEquals($store->min_confirmations, 1);
+        self::assertEquals($store->expiration_time, 15);
+        self::assertEquals($store->user_id, $user->id);
+
+    }
+}
