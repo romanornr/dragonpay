5b85dae1493d6904ae2311dfbab77151541f5d52
romanornr <romanornr@gmail.com>
2018-03-19 01:47:38 +0100

    test creation of masterwallets

--- database/factories/MasterwalletFactory.php
+++ database/factories/MasterwalletFactory.php
@@ -1,14 +1,18 @@
 <?php
 
 use Faker\Generator as Faker;
-use App\User;
+use App\Models\Store;
 
 $factory->define(App\Models\Masterwallet::class, function (Faker $faker){
+
+    $store = factory(Store::class)->create();
+
    return [
        'cryptocurrency_id' => 1,
-       'user_id' => factory(User::class)->create()->id,
-       'store_id' => $faker->numberBetween(0, 200),
-       'address_type' => $faker->numberBetween(15, 500),
-       'script_type' => ''
+       'user_id' => $store->user_id,
+       'store_id' => $store->id,
+       'address_type' => 'segwit',
+       'script_type' => 'p2sh',
+       'master_public_key' => 'xpub6DBfFoZHK5ZCzuoViVTzmRTf91DEVvYoifJQToHhHAwS2pmyeQCfQ5pqCg65WYBB2jnyDtoPRdpLVgwH5UpFswFX1qNtD4ccpZJXB9fqkQA'
    ];
 });
--- tests/Feature/MasterwalletTest.php
+++ tests/Feature/MasterwalletTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Cryptocurrency;
+use App\Models\Masterwallet;
 use App\Models\Store;
 use Tests\TestCase;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Database\Seeder;
 
 class MasterwalletTest extends TestCase
 {
@@ -14,7 +17,30 @@ class MasterwalletTest extends TestCase
 
     public function testCreateMasterwallet()
     {
+        $this->seed('CryptocurrenciesTableSeeder');
 
+        $store = factory(Store::class)->create();
+        $user = User::find($store->user_id)->first();
+        Auth::login($user);
+        $response = $this->get('/masterwallets/create');
+        $response->assertStatus(200);
+
+        $response = $this->post('/masterwallets', [
+            'store_id' => $store->id,
+            'cryptocurrency_id' => 1,
+            'address_type' => 'segwit',
+            'master_public_key' => 'xpub6DBfFoZHK5ZCzuoViVTzmRTf91DEVvYoifJQToHhHAwS2pmyeQCfQ5pqCg65WYBB2jnyDtoPRdpLVgwH5UpFswFX1qNtD4ccpZJXB9fqkQA',
+        ]);
+
+        $response->assertStatus(302);
+
+        $masterwallet = Masterwallet::find(1);
+        self::assertEquals($masterwallet->store_id, $store->id);
+        self::assertEquals($masterwallet->cryptocurrency_id, 1);
+        self::assertEquals($masterwallet->address_type, 'segwit');
+        self::assertEquals($masterwallet->script_type, 'p2sh');
+        self::assertEquals($masterwallet->user_id, $store->user_id);
+        
     }
 
 }
